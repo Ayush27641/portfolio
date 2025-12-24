aa5d24affaa76fa4944b9df1bf6aa74bcddbd8aa
Ayush27641 <bestayush3@gmail.com>
2025-12-24 06:27:47 +0530

    fix3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "C:/Users/Asus/Downloads/Portfolio-main/Portfolio-main",
+  },
   images: {
     remotePatterns: [
       {
